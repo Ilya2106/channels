cbb7a2ddf93d3b470819366d5c3ca1f3adbe9b1b
Ilya2106 <96929335+Ilya2106@users.noreply.github.com>
2022-12-04 13:05:18 +0300

    Update asgi.py

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -11,14 +11,13 @@ import os
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+django_asgi_app = get_asgi_application()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.auth import AuthMiddlewareStack
 import chat.routing
 
-
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
